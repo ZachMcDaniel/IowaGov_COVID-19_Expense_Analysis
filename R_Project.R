5f7358246a3b0973b281b50b57ee028c8930f8a6
Zach McDaniel <zachary-mcdaniel@uiowa.edu>
2020-11-11 12:45:29 -0600

    updated file

--- R_Project.R
+++ R_Project.R
@@ -1,1 +1,8 @@
-#groupproject
+rm(list = ls())
+
+###############################################################################
+#Downloading and Cleaning Data
+###############################################################################
+
+download.file('https://data.iowa.gov/api/views/68ay-29f3/rows.csv', "expense_data.csv")
+covid_df <- readr::read_csv("expense_data.csv")
